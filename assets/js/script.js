bea788f5908145ca4ae1725399e6f6716880ab44
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-07-09 15:50:55 +0800

    Refactor script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,40 @@ const appData = {
     weatherData: {},
 };
 
-const cityInput = document.querySelector('#city-input');
+const cityNameInput = document.querySelector('#city-name-input');
 const searchButton = document.querySelector('#search-button');
 
 const temperature = document.querySelector('#temperature');
 const weather = document.querySelector('#weather');
 
 searchButton.addEventListener('click', async function () {
-    const city = cityInput.value;
-    appData.cities = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
-    .then((res) => {
-        return res.json();
-    });
+    const cityName = cityNameInput.value;
+    
+    appData.cities = await getCities(cityName);
+    appData.weatherData = await getWeatherData(appData.cities[0]);
 
-    const lat = appData.cities[0].lat;
-    const lon = appData.cities[0].lon;
-    appData.weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
-    .then((res) => {
-        return res.json();
-    });
+    render();
+});
 
+function getCities(cityName) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+    
+    return fetch(url)
+        .then((res) => res.json());
+}
+
+async function getWeatherData(city) {
+    const lat = city.lat;
+    const lon = city.lon;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+    return fetch(url)
+        .then((res) => res.json());
+}
+
+function render() {
     const temp = Math.round(appData.weatherData.main.temp);
     const weat = appData.weatherData.weather[0].main;
     temperature.innerHTML = temp;
     weather.innerHTML = weat;
-});
+}
